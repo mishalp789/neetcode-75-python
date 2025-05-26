1ec343ca3a758fdafeebdaf30a2d6a000c2cab72
Muhammed Mishal <133393651+mishalp789@users.noreply.github.com>
2025-05-26 21:01:20 +0530

    Update Contains_Duplicate.py

---
+++ arrays/Contains_Duplicate.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
